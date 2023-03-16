1cd96df691945e866c41fc97daa076d68b708c62
izzy215 <jihyeon3@gmail.com>
2023-03-16 17:31:33 +0900

    폴더구조

---
+++ Final_test/src/main/java/com/boat/controller/main/HomeController.java
@@ -0,0 +1,38 @@
+package com.boat.controller.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HomeController {
+
+
+	
+	@GetMapping("/confer")
+	public String hello2() {
+		return "/Conference_Res/conferMain";
+	}
+	
+	@GetMapping("/write")
+	public String hello3() {
+		return "/Board/boardWrite";
+	}
+	
+	@GetMapping("/confer2")
+	public String hello4() {
+		return "/Conference_Res/calendar2";
+	}
+	
+	@GetMapping("/admin")
+	public String hello5() {
+		return "/Admin/admin_radio";
+	}
+	
+	@GetMapping("/address")
+	public String hello6() {
+		return "/Address/newaddress";
+	}
+	
+	
+}
