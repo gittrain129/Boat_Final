e0c3994a3dc57ae54be33881d75f5891010c2d5a
dmswjd37 <dmswjddid37@naver.com>
2023-03-29 15:51:03 +0900

    이메일 인증 작업중

--- Final_test/src/main/java/com/boat/Task/SendMail.java
+++ Final_test/src/main/java/com/boat/Task/SendMail.java
@@ -1,13 +1,20 @@
 package com.boat.Task;
 
 import java.io.File;
+import java.io.UnsupportedEncodingException;
+import java.util.Random;
 
+import javax.mail.MessagingException;
+import javax.mail.Message.RecipientType;
+import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.MailException;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.mail.javamail.MimeMessagePreparator;
@@ -79,4 +86,6 @@ public class SendMail {
 		mailSender.send(mp);	//메일 전송합니다.
 		Logger.info("메일 전송했습니다.");
 	}//sendMail
+	
+	
 }
