3d7408aba4ba3662a9688a9444cb7a5185864b2c
Kim Sojoong <Kim Sojoong@DESKTOP-FK0RMA1>
2023-03-31 17:30:00 +0900

    230331 Kimsj

--- Final_test/src/main/java/com/boat/mybatis/mapper/WorkboardMapper.java
+++ Final_test/src/main/java/com/boat/mybatis/mapper/WorkboardMapper.java
@@ -1,26 +1,28 @@
-package com.boat.controller.workboard;
+package com.boat.mybatis.mapper;
 
 import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 
+import com.boat.domain.Workboard;
+
 @Mapper
-public interface  WorkboardCommentMapper {
+public interface  WorkboardMapper {
 
 			//글의 갯수 구하기
 			public int getListCount(int board_num);
 			
 			//댓글 목록 가져오기
-			public List<WorkboardComment> getCommentList(Map<String, Integer> map);
+			public List<Workboard> getCommentList(Map<String, Integer> map);
 			
 			//댓글 등록하기
-			public int commentsInsert(WorkboardComment c);
+			public int commentsInsert(Workboard c);
 			
 			//댓글 삭제
 			public int commentsDelete(int num);
 			
 			//댓글 수정
-			public int commentsUpdate(WorkboardComment co);
+			public int commentsUpdate(Workboard co);
 
 }
