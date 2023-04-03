7d588d670556c5938a6c41850dccac77ea0790fa
izzy215 <jihyeon3@gmail.com>
2023-04-03 17:39:21 +0900

    도메인 게터세터

--- Final_test/src/main/java/com/boat/domain/Attandance.java
+++ Final_test/src/main/java/com/boat/domain/Attandance.java
@@ -1,107 +1,44 @@
 package com.boat.domain;
 
-import lombok.Data;
 
-@Data
 public class Attandance {
 
-	//MEMBER_ATTENDANCE
-//	1	NO	아이디
-//	2	EMPNO	사번
-//	3	ON_TIME	출근/복귀 시간
-//	4	OFF_TIME	퇴근시간
-//	5	OUT_TIME	외출시간
-	
 	private String EMPNO;
 	private String DEPT;
 	private String CONTENT;
 	private String ON_TIME;
 	private String OFF_TIME;
-	public String getEMPNO() {
-		return EMPNO;
-	}
-	public void setEMPNO(String eMPNO) {
-		EMPNO = eMPNO;
-	}
-	public String getDEPT() {
-		return DEPT;
-	}
-	public void setDEPT(String dEPT) {
-		DEPT = dEPT;
-	}
-	public String getCONTENT() {
-		return CONTENT;
-	}
-	public void setCONTENT(String cONTENT) {
-		CONTENT = cONTENT;
-	}
-	public String getON_TIME() {
-		return ON_TIME;
-	}
-	public void setON_TIME(String oN_TIME) {
-		ON_TIME = oN_TIME;
-	}
-	public String getOFF_TIME() {
-		return OFF_TIME;
-	}
-	public void setOFF_TIME(String oFF_TIME) {
-		OFF_TIME = oFF_TIME;
-	}
-	public String getOUT_TIME() {
-		return OUT_TIME;
-	}
-	public void setOUT_TIME(String oUT_TIME) {
-		OUT_TIME = oUT_TIME;
-	}
-	public String getREG_DATE() {
-		return REG_DATE;
-	}
-	public void setREG_DATE(String rEG_DATE) {
-		REG_DATE = rEG_DATE;
-	}
-	private String OUT_TIME;
-	private String REG_DATE;
-	public String getEMPNO() {
-		return EMPNO;
-	}
-	public void setEMPNO(String eMPNO) {
-		EMPNO = eMPNO;
-	}
-	public String getDEPT() {
-		return DEPT;
-	}
-	public void setDEPT(String dEPT) {
-		DEPT = dEPT;
-	}
-	public String getCONTENT() {
-		return CONTENT;
-	}
-	public void setCONTENT(String cONTENT) {
-		CONTENT = cONTENT;
-	}
-	public String getON_TIME() {
-		return ON_TIME;
-	}
-	public void setON_TIME(String oN_TIME) {
-		ON_TIME = oN_TIME;
-	}
-	public String getOFF_TIME() {
-		return OFF_TIME;
-	}
-	public void setOFF_TIME(String oFF_TIME) {
-		OFF_TIME = oFF_TIME;
-	}
-	public String getOUT_TIME() {
-		return OUT_TIME;
-	}
-	public void setOUT_TIME(String oUT_TIME) {
-		OUT_TIME = oUT_TIME;
-	}
-	public String getREG_DATE() {
-		return REG_DATE;
-	}
-	public void setREG_DATE(String rEG_DATE) {
-		REG_DATE = rEG_DATE;
-	}
 	
+
+	public String getEMPNO() {
+		return EMPNO;
+	}
+	public void setEMPNO(String eMPNO) {
+		EMPNO = eMPNO;
+	}
+	public String getDEPT() {
+		return DEPT;
+	}
+	public void setDEPT(String dEPT) {
+		DEPT = dEPT;
+	}
+	public String getCONTENT() {
+		return CONTENT;
+	}
+	public void setCONTENT(String cONTENT) {
+		CONTENT = cONTENT;
+	}
+	public String getON_TIME() {
+		return ON_TIME;
+	}
+	public void setON_TIME(String oN_TIME) {
+		ON_TIME = oN_TIME;
+	}
+	public String getOFF_TIME() {
+		return OFF_TIME;
+	}
+	public void setOFF_TIME(String oFF_TIME) {
+		OFF_TIME = oFF_TIME;
+	}
+
 }
--- Final_test/src/main/java/com/boat/domain/Member2.java
+++ Final_test/src/main/java/com/boat/domain/Member2.java
@@ -5,9 +5,7 @@ import java.util.List;
 
 import org.springframework.web.multipart.MultipartFile;
 
-import lombok.Data;
 
-@Data
 public class Member2 {
 	private String EMPNO;
 	private String DEPT;
@@ -15,7 +13,6 @@ public class Member2 {
 	private String PASSWORD;
 	private String NAME;
 	private String EMAIL;
-	
 	private MultipartFile uploadfile;
 	
 	private String PROFILE_FILE;
@@ -25,6 +22,86 @@ public class Member2 {
 	private String NAVERLOGIN;
 	private List<Todo> Todo;
 	
+	
+	public String getEMPNO() {
+		return EMPNO;
+	}
+	public void setEMPNO(String eMPNO) {
+		EMPNO = eMPNO;
+	}
+	public String getDEPT() {
+		return DEPT;
+	}
+	public void setDEPT(String dEPT) {
+		DEPT = dEPT;
+	}
+	public String getJOB() {
+		return JOB;
+	}
+	public void setJOB(String jOB) {
+		JOB = jOB;
+	}
+	public String getPASSWORD() {
+		return PASSWORD;
+	}
+	public void setPASSWORD(String pASSWORD) {
+		PASSWORD = pASSWORD;
+	}
+	public String getNAME() {
+		return NAME;
+	}
+	public void setNAME(String nAME) {
+		NAME = nAME;
+	}
+	public String getEMAIL() {
+		return EMAIL;
+	}
+	public void setEMAIL(String eMAIL) {
+		EMAIL = eMAIL;
+	}
+	public MultipartFile getUploadfile() {
+		return uploadfile;
+	}
+	public void setUploadfile(MultipartFile uploadfile) {
+		this.uploadfile = uploadfile;
+	}
+	public String getPROFILE_FILE() {
+		return PROFILE_FILE;
+	}
+	public void setPROFILE_FILE(String pROFILE_FILE) {
+		PROFILE_FILE = pROFILE_FILE;
+	}
+	public String getPROFILE_IMG() {
+		return PROFILE_IMG;
+	}
+	public void setPROFILE_IMG(String pROFILE_IMG) {
+		PROFILE_IMG = pROFILE_IMG;
+	}
+	public String getREGISTER_DATE() {
+		return REGISTER_DATE;
+	}
+	public void setREGISTER_DATE(String rEGISTER_DATE) {
+		REGISTER_DATE = rEGISTER_DATE;
+	}
+	public String getAUTH() {
+		return AUTH;
+	}
+	public void setAUTH(String aUTH) {
+		AUTH = aUTH;
+	}
+	public String getNAVERLOGIN() {
+		return NAVERLOGIN;
+	}
+	public void setNAVERLOGIN(String nAVERLOGIN) {
+		NAVERLOGIN = nAVERLOGIN;
+	}
+	public List<Todo> getTodo() {
+		return Todo;
+	}
+	public void setTodo(List<Todo> todo) {
+		Todo = todo;
+	}
+
 	
 	
 }
