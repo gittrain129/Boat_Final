60e973ded7a46f572172ed58b29e9d7f6aed2916
izzy215 <jihyeon3@gmail.com>
2023-04-10 16:34:52 +0900

    admin페이지 'admin' 로그인시 가능

--- Final_test/src/main/java/com/boat/controller/admin/adminController.java
+++ Final_test/src/main/java/com/boat/controller/admin/adminController.java
@@ -2,13 +2,22 @@ package com.boat.controller.admin;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@RequestMapping(value="/admin")
 public class adminController {
 
 
-	@GetMapping("/admin")
+	@GetMapping("/list")
+	public String adminlist() {
+		return "/Admin/adminlist";
+	}
+	
+	
+	
+	@GetMapping("")
 	public String hello5() {
 		return "/Admin/admin_radio";
 	}
