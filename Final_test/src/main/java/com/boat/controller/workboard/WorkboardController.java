2146036d00f73970a6f9bae20e612583ef205d95
Kim Sojoong <Kim Sojoong@DESKTOP-FK0RMA1>
2023-04-12 14:03:55 +0900

    230412 Kimsj 파일정리 및 수정

--- Final_test/src/main/java/com/boat/controller/workboard/WorkboardController.java
+++ Final_test/src/main/java/com/boat/controller/workboard/WorkboardController.java
@@ -61,13 +61,14 @@ public class  WorkboardController {
 		return WorkboardService.workboardInsert(co);		
 	}
 	
+	@ResponseBody
 	@PostMapping(value="/update")
 	public int CommentUpdate(Workboard co) {
 		return WorkboardService.workboardUpdate(co);				
 	}
 	
 	
-	
+	@ResponseBody
 	@PostMapping(value="/delete")
 	public int CommentDelete(int num) {
 		return WorkboardService.workboardDelete(num);				
